e61052d21fe8d43928c1a31e8b0c60ee2c5373d8
karnakar <karnakar@ymail.com>
2020-12-20 14:46:08 -0500

    initial setup

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+
+def calculate():
+        num1=input("enter num1")
+        num2=input("enter num2")
+        operand=input("enter operand")
+        value=0
+        print(operand)
+        if(operand=="+"):
+            value=int(num1)+int(num2)
+        
+        return value
+
+print(calculate())
+            
+            
+    
+    
